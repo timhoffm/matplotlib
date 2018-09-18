3dcc1dc834ba1173993d6221987f5799c3bf7213
Antony Lee <anntzer.lee@gmail.com>
2018-09-18 16:34:32 +0200

    Remove (most) APIs deprecated in mpl2.2.

--- lib/matplotlib/rcsetup.py
+++ lib/matplotlib/rcsetup.py
@@ -599,10 +599,6 @@ validate_legend_loc = ValidateInStrings(
 def validate_svg_fonttype(s):
     if s in ["none", "path"]:
         return s
-    if s == "svgfont":
-        cbook.warn_deprecated(
-            "2.2", "'svgfont' support for svg.fonttype is deprecated.")
-        return s
     raise ValueError("Unrecognized svg.fonttype string '{}'; "
                      "valid strings are 'none', 'path'")
 
